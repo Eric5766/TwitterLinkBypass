0c5aae6ffd99682dee66c92b20d865561fcc3de5
Eric <9409967+Eric5766@users.noreply.github.com>
2024-04-08 10:46:19 +0100

    Upload bypass.js

---
+++ bypass.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Bypass t.co links
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-08
+// @description  Where possible, bypass t.co links on click by taking the actual url from the link tag (The on-hover popup that shows the true url).  Does not work with links like Youtube videos where the destination URL is not stored anywhere client side.
+// @author       You
+// @match        *://twitter.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+function handleLinkClick(event) {
+    let clickedElement = event.target;
+
+    while ((clickedElement.tagName !== "A" || !clickedElement.href) && clickedElement.parentNode) {
+        clickedElement = clickedElement.parentNode;
+    }
+
+    if (clickedElement.tagName !== "A") return;
+
+    if (clickedElement.href.startsWith("https://t.co")) {
+        event.preventDefault();
+
+        let url = clickedElement.textContent.replace("…", "");
+
+        if (url.startsWith("http")) {
+            window.open(url, '_blank');
+        } else {
+            window.open(clickedElement.href, '_blank');
+        }
+    }
+}
+
+window.addEventListener("click", handleLinkClick);
+
+
